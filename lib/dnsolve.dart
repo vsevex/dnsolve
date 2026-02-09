ab04aa172cb241e60308ea2610c66e310fa273c4
vsevex <vsevex@gmail.com>
2026-02-09 10:47:17 +0400

    docs: update DNSolve documentation to reflect native resolution capabilities and export NativeResolver

--- lib/dnsolve.dart
+++ lib/dnsolve.dart
@@ -1,5 +1,6 @@
-/// Provider of an easy way of performing DNS lookups.
+/// Provider of an easy way of performing DNS lookups using native resolution.
 library;
 
 export 'src/dnsolve.dart';
 export 'src/exception.dart';
+export 'src/native_bindings.dart' show NativeResolver;
